645ca741c934a378acea98babfc0df64a07b6c11
Mary K <wildcat.3333@yandex.ru>
2023-09-23 13:42:26 +0700

    def parse parse_pep  items

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -86,3 +86,7 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+ITEM_PIPELINES = {
+    'pep_parse.pipelines.TotalPepPipeline': 300,
+}
--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -1,10 +1,23 @@
 import scrapy
 
+from pep_parse.items import PepParseItem
+
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
     allowed_domains = ['peps.python.org']
-    start_urls = ['http://peps.python.org/']
+    start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        pass
+        all_pep = response.css(
+            '#numerical-index a.pep.reference.internal::attr(href)')
+        for pep_link in all_pep:
+            yield response.follow(pep_link, callback=self.parse_pep)
+
+    def parse_pep(self, response):
+        data = {
+            'number': response.css('h1.page-title::text').get().split()[1],
+            'name': response.css('h1.page-title::text').get(),
+            'status': response.css('abbr::text').get(),
+        }
+        yield PepParseItem(data)
