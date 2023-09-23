46a1be7f63634b05556a02c8db10138995c0f41a
Mary K <wildcat.3333@yandex.ru>
2023-09-23 23:21:25 +0700

    review changes2

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -4,8 +4,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).parent
 RESULTS_DIR = 'results'
 DIR = BASE_DIR / RESULTS_DIR
-RESULTS = str(DIR) + '/'
-
 
 DATETIME_FORMAT = '%Y-%m-%dT%H-%M-%S'
 NOW_FORMATTED = dt.datetime.now().strftime(DATETIME_FORMAT)
@@ -13,7 +11,7 @@ NOW_FORMATTED = dt.datetime.now().strftime(DATETIME_FORMAT)
 HEADER_PEP = ('Статус', 'Количество')
 FIELDS_NAME = ['number', 'name', 'status']
 SUM_FILE_NAME = 'status_summary'
-PEP_FILE_NAME = 'pep_%(time)s.csv'
+PEP_FILE_NAME = 'pep_%(time)s'
 FILE_FORMAT = 'csv'
 MODE_OPEN_FILE = 'w'
 MODE_DOWNLOAD = 'wb'
@@ -21,15 +19,12 @@ CODE_PAGES = 'utf-8'
 
 BOT_NAME = 'pep_parse'
 
-NOW_FORMATTED = dt.datetime.now().strftime(DATETIME_FORMAT)
-
 SPIDER_MODULES = ['pep_parse.spiders']
 
 ROBOTSTXT_OBEY = True
 
 FEEDS = {
-    RESULTS + '/' + PEP_FILE_NAME: {
-    # 'results/' + PEP_FILE_NAME: {
+    RESULTS_DIR + '/' + PEP_FILE_NAME + '.' + FILE_FORMAT: {
         'format': FILE_FORMAT,
         'fields': FIELDS_NAME,
         'overwrite': True
