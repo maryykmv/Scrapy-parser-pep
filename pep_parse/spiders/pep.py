842aac70ab420f68860b2fb00c864edafa996b94
Mary K <wildcat.3333@yandex.ru>
2023-09-23 23:31:15 +0700

    review changes4

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -5,11 +5,11 @@ from pep_parse.items import PepParseItem
 
 class PepSpider(scrapy.Spider):
     name = 'pep'
-    allowed_domains = ['peps.python.org']
     start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        for pep_link in response.css('#numerical-index a.pep.reference.internal::attr(href)'):
+        for pep_link in (response.css(
+                '#numerical-index a.pep.reference.internal::attr(href)')):
             yield response.follow(pep_link, callback=self.parse_pep)
 
     def parse_pep(self, response):
