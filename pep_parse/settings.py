0496bf6f25b2ddde66457923dc87b95f9ffbf008
Mary K <wildcat.3333@yandex.ru>
2023-09-23 15:44:23 +0700

    changes settings

--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,92 +1,40 @@
-# Scrapy settings for pep_parse project
-#
-# For simplicity, this file contains only settings considered important or
-# commonly used. You can find more settings consulting the documentation:
-#
-#     https://docs.scrapy.org/en/latest/topics/settings.html
-#     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
+# import datetime as dt
+
+# from .constants import (BASE_DIR, RESULTS_DIR, FILE_FORMAT,
+#                         FIELDS_NAME, PEP_FILE_NAME, DATETIME_FORMAT)
+
+from .constants import (FILE_FORMAT, FIELDS_NAME)
 
 BOT_NAME = 'pep_parse'
+# DIR = str(BASE_DIR / RESULTS_DIR)
+# NOW_FORMATTED = dt.datetime.now().strftime(DATETIME_FORMAT)
+
 
 SPIDER_MODULES = ['pep_parse.spiders']
 NEWSPIDER_MODULE = 'pep_parse.spiders'
 
-
-# Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'pep_parse (+http://www.yourdomain.com)'
-
-# Obey robots.txt rules
 ROBOTSTXT_OBEY = True
 
-# Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+FEEDS = {
+    'results/pep_%(time)s.' + FILE_FORMAT: {
+        'format': FILE_FORMAT,
+        'fields': FIELDS_NAME,
+        'overwrite': True
+    }
+}
 
-# Configure a delay for requests for the same website (default: 0)
-# See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
-# See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
-# The download delay setting will honor only one of:
-#CONCURRENT_REQUESTS_PER_DOMAIN = 16
-#CONCURRENT_REQUESTS_PER_IP = 16
-
-# Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
-
-# Disable Telnet Console (enabled by default)
-#TELNETCONSOLE_ENABLED = False
-
-# Override the default request headers:
-#DEFAULT_REQUEST_HEADERS = {
-#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-#   'Accept-Language': 'en',
-#}
-
-# Enable or disable spider middlewares
-# See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'pep_parse.middlewares.PepParseSpiderMiddleware': 543,
-#}
-
-# Enable or disable downloader middlewares
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'pep_parse.middlewares.PepParseDownloaderMiddleware': 543,
-#}
-
-# Enable or disable extensions
-# See https://docs.scrapy.org/en/latest/topics/extensions.html
-#EXTENSIONS = {
-#    'scrapy.extensions.telnet.TelnetConsole': None,
-#}
-
-# Configure item pipelines
-# See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'pep_parse.pipelines.PepParsePipeline': 300,
-#}
-
-# Enable and configure the AutoThrottle extension (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/autothrottle.html
-#AUTOTHROTTLE_ENABLED = True
-# The initial download delay
-#AUTOTHROTTLE_START_DELAY = 5
-# The maximum download delay to be set in case of high latencies
-#AUTOTHROTTLE_MAX_DELAY = 60
-# The average number of requests Scrapy should be sending in parallel to
-# each remote server
-#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
-# Enable showing throttling stats for every response received:
-#AUTOTHROTTLE_DEBUG = False
-
-# Enable and configure HTTP caching (disabled by default)
-# See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
-#HTTPCACHE_ENABLED = True
-#HTTPCACHE_EXPIRATION_SECS = 0
-#HTTPCACHE_DIR = 'httpcache'
-#HTTPCACHE_IGNORE_HTTP_CODES = []
-#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+# если делать через константы падает тест
+#  FAILED tests/test_settings.py::test_settings_feeds - AssertionError: 
+# Убедитесь, что в ключе словаря `FEEDS` перед именем файла 
+# указан путь к директории `results/`
+# FEEDS = {
+#     DIR + '/' + PEP_FILE_NAME + str(NOW_FORMATTED) + '.' + FILE_FORMAT: {
+#         'format': FILE_FORMAT,
+#         'fields': FIELDS_NAME,
+#         'overwrite': True
+#     }
+# }
 
 ITEM_PIPELINES = {
-    'pep_parse.pipelines.TotalPepPipeline': 300,
+    'pep_parse.pipelines.PepParsePipeline': 300,
 }
